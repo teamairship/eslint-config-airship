9acf6ff041ab3fb4df17391b8fb8890d84b6a256
Alex Hinson <alex@Alexs-MacBook-Pro-2.local>
2017-06-16 14:14:28 -0500

    Add class-methods-use-this rule

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ module.exports = {
 
     // http://eslint.org/docs/rules/comma-dangle
     'comma-dangle': [2, 'always-multiline'],
+
+    // http://eslint.org/docs/rules/class-methods-use-this
+    'class-methods-use-this': [0],
   },
 };
