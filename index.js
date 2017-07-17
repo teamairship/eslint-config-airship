b9aec614b2371238420b2e4eeef11bf4d6a23dc3
Alex Hinson <alex@Alexs-MacBook-Pro-2.local>
2017-07-17 08:58:08 -0500

    Add more rules

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ module.exports = {
   extends: require.resolve('eslint-config-airbnb-base'),
   rules: {
 
-    // http://eslint.org/docs/rules/comma-dangle
     'comma-dangle': [2, 'always-multiline'],
 
-    // http://eslint.org/docs/rules/class-methods-use-this
     'class-methods-use-this': [0],
+
+    'arrow-body-style': ['error', 'always'],
+
+    'arrow-parens': ['error', 'as-needed'],
+
+    'no-param-reassign': ['error', { props: false }],
+
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+
+    'max-len': ['error', 130],
   },
 };
